21eeba313be584ad6c56e5e0a5bd7cee4a43403b
Steven Polido <steven.polido@vistage.com>
2021-02-08 21:45:26 -0800

    Added HashLink around Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Navbar } from './Navbar';
 import { NavButton } from './NavButton';
 import LogoHeader from '../../public/logo_header.png';
+import { HashLink as Link } from 'react-router-hash-link';
 
 export const Header: React.FC = () => {
     return (
         <header className="yf__top-header">
             <div className="container">
-                <a href="#home"><img src={LogoHeader} /></a>
+                <Link to="#home"><img src={LogoHeader} /></Link>
                 <Navbar></Navbar>
-                {/* <NavButton></NavButton> */}
+                <NavButton></NavButton>
             </div>
         </header>
     )
