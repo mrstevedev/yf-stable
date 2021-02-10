2c9a2df95dd55ab6ad45ea76e04de3a22db40d65
Steven Polido <steven.polido@vistage.com>
2021-02-09 21:28:18 -0800

    Updated logo

--- yf_dashboard/src/components/Header.tsx
+++ yf_dashboard/src/components/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import LogoHeader from '../../public/logo_header.png';
+import LogoHeader from '../../public/logo_header.jpg';
 
 export const Header: React.FC = () => {
     return (
         <header className="yf__top-header dashboard">
             <div className="container">
                 <a href="http://localhost:8081/#">
-                    <img src={LogoHeader} />
+                    <img src={LogoHeader} width="90" height="90" />
                 </a>
             </div>
         </header>
