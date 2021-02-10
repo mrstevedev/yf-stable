13cde4515f03ccfdf988202d333822a9dbbd11c4
Steven Polido <steven.polido@vistage.com>
2021-02-09 21:27:28 -0800

    Added typings for jpg

--- yf_dashboard/src/import-png.d.ts
+++ yf_dashboard/src/import-png.d.ts
@@ -1,4 +1,9 @@
 declare module "*.png" {
-    const value: any;
-    export default value;
-  }
+  const value: any;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
