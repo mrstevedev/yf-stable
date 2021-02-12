088862b5783a320c998ccda4451cba1e70475293
Steven Polido <steven.polido@vistage.com>
2021-02-12 13:56:25 -0800

    Added dimensions to logo

--- yf_front/src/components/Header.tsx
+++ yf_front/src/components/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Navbar } from './Navbar';
 import { NavButton } from './NavButton';
-import LogoHeader from '../../public/logo_header.png';
+import LogoHeader from '../../public/logo_header.jpg';
 import { HashLink as Link } from 'react-router-hash-link';
 
 export const Header: React.FC = () => {
     return (
         <header className="yf__top-header">
             <div className="container">
-                <Link to="#home"><img src={LogoHeader} /></Link>
+                <Link to="/"><img src={LogoHeader} width="90" height="90" /></Link>
                 <Navbar></Navbar>
                 <NavButton></NavButton>
             </div>
