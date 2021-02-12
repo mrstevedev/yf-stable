b95238f0b4b73a03877d4f10a3c636cc7212bd9b
Steven Polido <steven.polido@vistage.com>
2021-02-12 13:46:18 -0800

    Added height to images

--- yf_front/src/components/UseCases.tsx
+++ yf_front/src/components/UseCases.tsx
@@ -11,17 +11,17 @@ export const UseCases: React.FC = () => {
                 <p>Where rebase and seignorage tokens have failed with their fluctuating balances that consistently decrease holder equity, YFStable functions to maintain balance while always increasing holder equity as the token finds its stability in the long-term. By doing this, the need for YFStable in the defi space becomes apparent, serving as both collateral as well as a lendable asset.</p>
                 <div className="yf__usecases-bottom">
                     <div className="yf__usecases-bottom-col">
-                        <img src={value} height="72" />
+                        <img src={value} width="79" height="72" />
                         <h2>Store of Value</h2>
                         <p>By consistently increasing holders equity, YFStable creates a system of increased value storage, even through moments of contraction.</p>
                     </div>
                     <div className="yf__usecases-bottom-col">
-                        <img src={hedge} height="72" />
+                        <img src={hedge} width="70" height="72" />
                         <h2>Hedging Against Market Movements</h2>
                         <p>Through YFStables algorithm which provides a stable market, it offers a hedge against extreme market movements for a large number of assets.</p>
                     </div>
                     <div className="yf__usecases-bottom-col">
-                        <img src={safety} height="72" />
+                        <img src={safety} width="57" height="72" />
                         <h2>A Decentralized Safety Net</h2>
                         <p>Through its system which works to ensure its value doesn’t spike and crash in cycles, its stability becomes a safe haven for users in the defi space.</p>
                     </div>
